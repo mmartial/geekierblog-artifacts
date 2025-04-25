4d05bbcdef33132bc62fe7843c31368e0ecfa8cc
Martial Michel <7586284+mmartial@users.noreply.github.com>
2025-04-24 20:56:51 -0400

    script fix

--- 202504wip-u24_openstack/newproject.sh
+++ 202504wip-u24_openstack/newproject.sh
@@ -4,12 +4,10 @@
 MY_PROJECT_ID=$(openstack project list | awk '/ newprojectname / {print $2}')
 MY_SEC_GROUP=$(openstack security group list --project ${MY_PROJECT_ID} | awk '/ default / {print $2}')
 # check values are assigned
-if [ -z $MY_PROJECT_ID]; or [ -z $MY_SEC_GROUP ]; then
+if [ -z $MY_PROJECT_ID ] || [ -z $MY_SEC_GROUP ]; then
   echo "ERROR: issue with needed variables"
   exit 1
 fi
-
-
 
 openstack security group rule create --ingress --ethertype IPv4 --protocol icmp ${MY_SEC_GROUP}
 openstack security group rule create --ingress --ethertype IPv4 --protocol tcp --dst-port 22 ${MY_SEC_GROUP}
